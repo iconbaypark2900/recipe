fe1a74982753c4b2cf1de3be796606c39a5636ff
iconbaypark2900 <jonaston015@gmail.com>
2023-07-29 20:00:07 -0400

    setting up spring

---
+++ src/test/java/com/calypso/recipe/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calypso.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
